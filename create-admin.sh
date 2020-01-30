464a17d08357a60be030dd4ca6a3aa325b9ad946
Fabio José <fabiojose@gmail.com>
2020-01-30 10:55:50 -0300

    To create the admin and role
    Signed-off-by: Fabio José <fabiojose@gmail.com>

---
+++ create-admin.sh
@@ -0,0 +1,9 @@
+kubectl create -f admin.user.yml
+kubectl create -f admin.user.role.yml
+
+# to get the token for the user above
+kubectl -n kube-system describe secret \
+        $(kubectl -n kube-system get secret | grep admin-user | awk '{print $1}')
+
+# to get the token
+kops get secrets kube --type secret -oplaintext
