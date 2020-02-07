921aad95bf9a792786708c5700731c0f63b4c647
Fabio José <fabiojose@gmail.com>
2020-02-07 20:51:20 -0300

    Update the dashboard to latest version
    Signed-off-by: Fabio José <fabiojose@gmail.com>

--- create-kubedash.sh
+++ create-kubedash.sh
@@ -1,17 +1,16 @@
-kubectl create \
-       -f https://raw.githubusercontent.com/kubernetes/kops/master/addons/kubernetes-dashboard/v1.8.3.yaml
+kubectl apply \
+        -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0-rc3/aio/deploy/recommended.yaml
 
 # To expose the dashboard with public dns
 kubectl expose deployment kubernetes-dashboard \
         --type=LoadBalancer \
-        --name=kube-dash \
-        -n kube-system
+        --name=kubernetes-dashboard-public \
+        -n kubernetes-dashboard
 
 # Ref: https://github.com/kubernetes/dashboard/wiki/Creating-sample-user
 # To see the public url
-kubectl describe svc kube-dash -n kube-system
-
-#kubectl get svc -n kube-system
+kubectl describe svc kubernetes-dashboard-public \
+       -n kubernetes-dashboard
 
 # to serve de dashboard at http://localhost:8001/api/v1/namespaces/kube-system/services/https:kubernetes-dashboard:/proxy/
 ##kubectl proxy
